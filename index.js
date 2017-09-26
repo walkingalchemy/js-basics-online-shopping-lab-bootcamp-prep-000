de314e0fb4ff2add5fdad050bb82f09878df688c
James Royer <walkingalchemy@gmail.com>
2017-09-26 17:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
-    let cartTotal = total(cart)
-    `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
+    `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
   }
   return cart
